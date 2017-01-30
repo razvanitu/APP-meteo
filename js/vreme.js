7f4718a57037638b8eb9b14db267a5078277ce2e
Razvan Itu <razwy.1107@yahoo.com>
2017-01-30 17:13:09 +0200

    3 functions split , toggle button almost functional.

--- js/vreme.js
+++ js/vreme.js
@@ -1,6 +1,7 @@
 var API_KEY = "b71b80fc0a8db2f9bafb5ec6d7e07c13";
 
-
+var cel = false;
+var wd;
 
 function displayTemp(fTemp,c){
 
@@ -10,29 +11,13 @@ function displayTemp(fTemp,c){
 
 }
 
-//function render (wd ,cel)
-  //var currentTemp = displayTemp(wd.weather[0], cel);
-  //  var high = displayTemp (wd.main.temp_max , cel);
-
-  //  var low = displayTemp (wd.main.temp_min, cel);
-
-$(function(){
-
-    var loc;
-
-    $.getJSON('http://ipinfo.io',function (d){
-
-        loc = "45.78,24.15".split(','); // d.loc.split(",");
-        console.log(loc);
-
-        $.getJSON('http://api.openweathermap.org/data/2.5/weather?units=metric&lat='
-            +loc[0]+ '&lon='+ loc[1] +'&APPID=' + API_KEY, function(wd){
-                console.log("got the data ,",wd);
+function render (wd ,cel){
+    
                 var currentLocation = wd.name;
                 var currentWeather = wd.weather[0].description;
-                var currentTemp = wd.weather[0].description;
-                var high = wd.main.temp_max;
-                var low = wd.main.temp_min;
+                var currentTemp = displayTemp(wd.main.temp, cel);
+                var high = displayTemp(wd.main.temp_max,cel);
+                var low = displayTemp (wd.main.temp_min, cel);
                 var icon = wd.weather[0].icon;
                 
 
@@ -44,27 +29,35 @@ $(function(){
                 var iconSrc = "http://openweathermap.org/img/w/" +icon + ".png";
             
                 $('#currentTemp').prepend('<img src=' + iconSrc +">");
+}
+
+$(function(){
+
+    var loc;
+
+    $.getJSON('http://ipinfo.io',function (d){
+
+        loc = "45.78,24.15".split(','); // d.loc.split(",");
+        console.log(loc);
+
+        $.getJSON('http://api.openweathermap.org/data/2.5/weather?units=metric&lat='
+            +loc[0]+ '&lon='+ loc[1] +'&APPID=' + API_KEY, function(apiData){
+                console.log("got the data ,",wd);
+                wd = apiData;
+                console.log(cel);
+                render (apiData , cel);
+                    $("#toggle").click(function(displayTemp){
+                        $("p").toggle();
+    cel =!cel;
+    render=(wd,cel);
+    
+                    
+                    })
+    
+
+    })
         })
     })
-});
 
-//$(function(){
 
-//var loc;
 
-//$.getJSON('http://ipinfo.io',function (d){
-
-//loc = d.loc.split(",");
-//console.log(loc);
-//$.getJSON('http://api.openweathermap.org/data/2.5/weather?units=metric&lat='
-//+loc[0]+ '&lon='+ loc[1] +'&APPID=' + API_KEY, function(apiData){
-
-    //wd = apiData;
-
-    $('toggle').click(function()
-    
-    
-    {
-cel =!cel;
-
-    })
