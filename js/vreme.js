6d1b4515a313d3d589fbbed26ee01d7e08a9e223
Razvan Itu <razwy.1107@yahoo.com>
2017-02-03 18:44:23 +0200

    Full app ,pics , css

--- js/vreme.js
+++ js/vreme.js
@@ -1,74 +1,83 @@
-var API_KEY = "b71b80fc0a8db2f9bafb5ec6d7e07c13";
-var cel = true;
-var wd;
-var loc = "45.78,24.15".split(',');
+var APPID = "b71b80fc0a8db2f9bafb5ec6d7e07c13";
+var units = "metric";
+var temp;
+var loc;
+var icon;
+var wind;
+var conditions;
 
-if("geolocation" in navigator)
-{
 
-    navigator.geolocation.getCurrentPosition(function(position){
-    
-});
-}
-function displayTemp (fTemp, c){
-  if(c) return Math.round((fTemp -32)* (5/9)) + " C";
-  return Math.round(fTemp) + "F"
-}
-
-function render (wd, cel){
-    
-    var currentLocation = wd.name;
-    var currentWeather = wd.weather[0].description;
-    var currentTemp = displayTemp(wd.main.temp, cel);
-    var high = displayTemp(wd.main.temp_max, cel);
-    var low = displayTemp (wd.main.temp_min, cel);
-    var icon = wd.weather[0].icon;
-
-    $('#currentLocation').html(currentLocation);
-    $('#currentTemp').html(currentTemp);
-    $('#currentWeather').html(currentWeather);
-    $('#high-low').html(high +"/" +low);
-
+function update(weather) {
     var iconSrc = "http://openweathermap.org/img/w/" +icon + ".png";
-    $('#currentTemp').prepend('<img src=' + iconSrc +">");
+    icon.src = "imgs/codes/" + weather.code + ".png"
+    wind.innerHTML = weather.wind;
+    conditions.innerHTML = weather.conditions;
+    loc.innerHTML = weather.location;
+    temp.innerHTML = weather.temp;
+    humidity.innerHTML = weather.humidity;
 }
 
-function fetchWeatherFromAPI () {
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?units=imperial&lat='
-        +loc[0]+ '&lon='+ loc[1] +'&APPID=' + API_KEY, function(apiData){
-            wd = apiData;
-            render(wd, cel);
-    });
+var showPositionBycity = function() {
+    var city = window.prompt("City name,Country code ?");
+    updateByCity(city);
 }
 
-
-$(function(){
-
-    fetchWeatherFromAPI();
-
-    $("#toggle").click(function(){
-        cel =!cel;
-        render(wd, cel);
-    });
-    $("#refresh").click(function(){
-        fetchWeatherFromAPI();
-        
-    });
-    $('#g01-01').on('change',function(){
-    var_val = $(this).is(':checked') ? 'checked' : 'unchecked';
-    console.log(var_val);
-    if (var_val === "unchecked"){
-        console.log("ssssaz");
-    }
-    else
-     
-     {
-         console.log("ssssaz22222222222");
-         setInterval(function(){ fetchWeatherFromAPI() }, 60000);
-     }
-    //setTimeout(function(){ fetchWeatherFromAPI() }, 3000);
+window.onload = function () {
+    temp = document.getElementById("temperature");
+    loc = document.getElementById("location");
+    icon = document.getElementById("icon");
+    wind = document.getElementById("wind");
+    conditions = document.getElementById("conditions");
+    humidity = document.getElementById("humidity");
     
-});
-})
+
+    /* geolocation*/
+    if(navigator.geolocation){
+	    var showPosition = function(position){
+            updateByGeo(position.coords.latitude, position.coords.longitude);
+        }
+	    navigator.geolocation.getCurrentPosition(showPosition, showPositionBycity);
+    } else {
+        showPositionBycity();
+    }
+}
 
 
+
+function updateByGeo(lat, lon){
+    var url = "http://api.openweathermap.org/data/2.5/weather?" +
+	"units=" + units +
+    "&lat=" + lat +
+	"&lon=" + lon +
+    "&APPID=" + APPID;
+    sendRequest(url);
+}
+
+
+function updateByCity(city){
+    var url = "http://api.openweathermap.org/data/2.5/weather?" +
+	"q=" + city +
+    "&units=" + units +
+	"&APPID=" + APPID;
+    sendRequest(url);
+}
+
+
+function sendRequest(url){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var data = JSON.parse(xmlhttp.responseText);
+            var weather = {};
+            weather.code = data.weather[0].id;
+            weather.conditions = data.weather[0].description;
+            weather.wind = data.wind.speed;
+            weather.location = data.name;
+            weather.temp = data.main.temp;
+            weather.humidity = data.main.humidity;
+            update(weather);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();    
+}
